26d62738ca2919194f43974714d690b9ed70cacc
saif-arshad <saifurrehman.web@gmail.com>
2025-09-05 06:15:23 -0700

    change the backend for compatible for vercel

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import morgan from "morgan";
 import "dotenv/config";
 
 const app = express();
-const PORT = 8000;
 
 app.use(cors({ origin: "*", credentials: true, optionsSuccessStatus: 202 }));
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 
+// your routes
 import routes from "./routes/main.routes.js";
 app.use("/api", routes);
 
@@ -21,6 +21,6 @@ app.use((req, res) => {
   return res.status(404).send({ message: "Route not found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default (req, res) => {
+  app(req, res);
+};
